01dc0d91ef166d415c30e061724a548b1f4b754a
navneetzz <navneetnnavneet1@gmail.com>
2017-07-05 15:58:52 +0530

    Added code simple search to the hackerearth Algorithm track

--- src/Algorithms/Searching/LinearSearch/SimpleSearch.java
+++ src/Algorithms/Searching/LinearSearch/SimpleSearch.java
@@ -1,7 +1,24 @@
 package Algorithms.Searching.LinearSearch;
 
+import java.util.*;
+
 /**
  * Created by Navneet Sharma (navneetzz) ryu on 5/7/17.
  */
 public class SimpleSearch {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++)
+            arr[i] = in.nextInt();
+        int k = in.nextInt();
+        for (int i=0;i<n;i++) {
+            if (arr[i]==k) {
+                System.out.println(i);
+                break;
+            }
+        }
+        in.close();
+    }
 }
