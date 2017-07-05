3bdd39fb762b7c84bc4bd90378d0367855c5f7bb
navneetzz <navneetnnavneet1@gmail.com>
2017-07-05 18:12:21 +0530

    Added code save patients to the hackerearth Algorithm track

---
+++ src/Algorithms/Searching/Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package Algorithms.Searching.Sorting;
+
+import java.util.*;
+/**
+ * Created by Navneet Sharma (navneetzz) ryu on 5/7/17.
+ */
+public class BubbleSort {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++)
+            arr[i] = in.nextInt();
+        int count=0;
+        for (int i=0;i<n-1;i++) {
+            for (int j=0;j<n-i-1;j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
