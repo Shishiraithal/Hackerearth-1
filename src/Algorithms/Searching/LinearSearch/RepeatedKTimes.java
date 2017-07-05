980be73ce9d77cc3315cab11b6459749c556a17e
navneetzz <navneetnnavneet1@gmail.com>
2017-07-05 15:25:18 +0530

    Added code The normal type to the hackerearth Algorithm track

---
+++ src/Algorithms/Searching/LinearSearch/RepeatedKTimes.java
@@ -0,0 +1,12 @@
+package Algorithms.Searching.LinearSearch;
+
+import java.util.*;
+
+/**
+ * Created by Navneet Sharma (navneetzz) ryu on 5/7/17.
+ */
+public class RepeatedKTimes {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
